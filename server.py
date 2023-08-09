9095ace54ee4c0a3f8838e046b16e823c52e325c
Shaun Mwangi <Shaun.mwangi@student.moringaschool.com>
2023-08-09 22:54:22 +0300

    Receive method

--- server.py
+++ server.py
@@ -30,3 +30,25 @@ def handle(client):
             broadcast(f'{nickname} left the chat!'.encode('ascii'))
             nicknames.remove(nickname)
             break
+
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f'Connected with {str(address)}')
+
+        client.send('SHAUN'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f'Nickname of client is {nickname}')
+        broadcast(f'{nickname} joined the chat!'.encode('ascii'))
+        client.send('Connected to the server'.encode('ascii'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+print('Server is listening... ')
+receive()
